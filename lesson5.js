129bde7498091c0e60ab614a42ccc09612a7d52c
arudets <49208516+arudets@users.noreply.github.com>
2019-04-03 09:36:39 +0300

    Update lesson5.js

--- lesson5.js
+++ lesson5.js
@@ -2,14 +2,22 @@
 var userEntry    = "TeST"
     ,captchaText = "test"
     ,captchaOk   = (userEntry == captchaText) || false
-    ;
-    
+    ;  
 console.log("captcha", captchaOk)
 
 
 // задание 1
-var string = "test1 test2 test3";
-
+var string = "test1 test2 test3"
+    ;
 console.log("slice:", string.slice(0, 5));
 console.log("substr:", string.substr(0, 4));
 console.log("substring", string.substring(5, 0));
+
+// задание 2
+var string     = "я люблю javaScript"
+    ,aString    =  string.split(" ")
+	;	
+console.log("К-во слов в строке:", aString.length);
+console.log("К-во символов с пробелом:", string.length);
+console.log("К-во символов без пробелов:", aString.join("").length);
+console.log("Замена \"люблю\" на \"обожаю\":", string.replace("люблю", "обожаю"));
