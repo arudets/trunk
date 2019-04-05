81c2afec864259fdbaf040150b4202e8d8dc7228
arudets <49208516+arudets@users.noreply.github.com>
2019-04-05 15:25:08 +0300

    Create lesson.js

---
+++ lesson.js
@@ -0,0 +1,14 @@
+// положительные через одно
+
+var array  = [1, 3, 5, -4, 8, 9, -8]   
+    ,result = "" 
+    ;
+    for(i = 0, j = 0; i < array.length; i++){
+		if(array[i] > 0){		    
+			if (!(j%2))	{
+			result += array[i] + ", ";	
+			}				
+			j ++;
+}
+	}
+console.log(result);
