61f9ab564d49e6b2723a15f78503f8b4af8e6e6c
arudets <49208516+arudets@users.noreply.github.com>
2019-04-05 15:30:28 +0300

    Update lesson.js

--- lesson.js
+++ lesson.js
@@ -11,3 +11,28 @@ var array  = [1, 3, 5, -4, 8, 9, -8]
     }
 	}
 console.log(result);
+
+// задание 1. пароль
+var pswdLength       = 8
+    ,pswd            = ""
+    ,randDiapasonNum = 0	
+    ,diapasons = {
+	    0  : "number"
+	   ,1 : "capital letter"
+	   ,2 : "small letter"
+	}
+    ,sympols = {
+		"number"          : [48, 57]
+		,"capital letter" : [65, 90] 
+		,"small letter"   : [97, 122]		
+	}
+    ;
+	
+for(var i = 0; i < pswdLength; i ++){
+  randDiapasonNum = Math.floor(Math.random() * 3);
+  firstSymbolNum = sympols[diapasons[randDiapasonNum]][0];
+  lastSymbolNum = sympols[diapasons[randDiapasonNum]][1];
+  pswd += String.fromCharCode(Math.floor(Math.random() * (lastSymbolNum - firstSymbolNum + 1)) + firstSymbolNum);  	
+}
+
+console.log("pswd:", pswd)
