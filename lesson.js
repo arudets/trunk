c9b92cd47d5855a5ff85ee61b0218e7ef3ed9ed5
arudets <49208516+arudets@users.noreply.github.com>
2019-04-05 15:27:50 +0300

    Update lesson.js

--- lesson.js
+++ lesson.js
@@ -1,11 +1,10 @@
 // положительные через одно
-
 var array  = [1, 3, 5, -4, 8, 9, -8]   
     ,result = "" 
     ;
     for(i = 0, j = 0; i < array.length; i++){
 		if(array[i] > 0){		    
-			if (!(j%2))	{
+			if (!(j%2)){
 			result += array[i] + ", ";	
 			}				
 		j ++;
