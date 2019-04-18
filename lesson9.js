ea47782f70164bee159d3e3d9a3041f23d8cf72a
arudets <49208516+arudets@users.noreply.github.com>
2019-04-18 12:36:09 +0300

    Update lesson9.js

--- lesson9.js
+++ lesson9.js
@@ -1,28 +1,26 @@
 // транслит преобразовать в класс
 
 function Translit(message){
-	this.message = message;
-	this.translatedMessage = '';
+   this.message           = message;
+   this.translatedMessage = '';
 	
-    this.translate = function(){
-	   var symbols  = {
-		  "и" : "i"
+   this.translate = function(){
+       var symbols  = {
+	     "и" : "i"
 	     ,"в" : "v"
 	     ,"а" : "a"
 	     ,"н" : "n"
 	     ,"о" : "o"
 	     ,"м" : "m"
 	     ,"к" : "k"		
-	    };
+	      };
 	
-	  this.message = this.message.toLowerCase();
-	  for (var i = 0; i < this.message.length; i++){
-		  this.translatedMessage += symbols[this.message[i]] ? symbols[this.message[i]] : this.message[i] // если нет символа то undefine который приводиться к false
-		
-	    }
-	
-    console.log (this.message, this.translatedMessage);	
-	}	
+      this.message = this.message.toLowerCase();
+      for (var i = 0; i < this.message.length; i++){
+           this.translatedMessage += symbols[this.message[i]] ? symbols[this.message[i]] : this.message[i] 		
+           }	
+      console.log (this.message, this.translatedMessage);	
+   }	
 }
 
 translit1 = new Translit("Иванов Максим");
@@ -31,20 +29,18 @@ translit1.translate();
 
 //класс машина
 function Car(color, mobility, fuelType){
-	this.color = color;
-	this.mobility = mobility;	
-	this.fuelType = fuelType;
+    this.color    = color;
+    this.mobility = mobility;	
+    this.fuelType = fuelType;
 	
     this.fuelConsumption = function(){
-		console.log("Расход топлива ")		
+	console.log("Расход топлива ")		
 	}
-
     this.speed = function()	{
-		console.log("Эх прокачу!")
+	console.log("Эх прокачу!")
 	}
 }
 
 car1 = new Car("white", 10, "petrol");
-
 car1.fuelConsumption();
 car1.speed();
